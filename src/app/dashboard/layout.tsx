6f87f4706993a61e28d1f00d9d78801d8ab36c6d
gpbezerra <gabrielpbezerra8@gmail.com>
2024-03-13 21:34:32 -0300

    starting pages

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen">
+      <div className="">
+        {/* <SideNav /> */}
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
